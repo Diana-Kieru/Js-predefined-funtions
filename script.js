f040a0510f2a3175cfd2f6dc5f8ea27b94fdfb93
diana <dianakieru1@gmil.com>
2022-10-14 10:28:06 +0300

    created funtions n javascript

---
+++ script.js
@@ -0,0 +1,48 @@
+// pre defined funtions and examples
+// eval() function example
+
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = eval(text);
+console.log(result);
+
+isFinite()
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = isFinite(text);
+console.log(result);
+
+isNaN()
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = isNaN(text);
+console.log(result);
+
+Number()
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = Number(text);
+console.log(result);
+
+parseFloat()
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = parseFloat(text);
+console.log(result);
+
+parseInt()
+let x = 10;
+let y = 20;
+let text = "x * y";
+let result = parseInt(text);
+console.log(result);
+
+
+
+
+
